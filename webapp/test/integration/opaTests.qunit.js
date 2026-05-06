3d5503d61f8f013612a82c35fb8e0bc9f7190a34
alf4th <rosyid.alfatah.work@gmail.com>
2026-05-06 11:03:30 +0700

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["zdelivuptime/delivuptimelabelqr/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"zdelivuptime/delivuptimelabelqr/test/unit/controller/ZC_INB_DELIV_UPTIME_LABELQR.controller"
-], function () {
-	"use strict";
-});
